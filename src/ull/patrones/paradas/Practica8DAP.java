c10bc5828737c04b732d53f71cbe49ccf43dc1bb
Orlandy Ariel <alu0100773408@ull.edu.es>
2016-11-25 13:44:20 +0000

    clase principal con método main

---
+++ src/ull/patrones/paradas/Practica8DAP.java
@@ -0,0 +1,12 @@
+package ull.patrones.paradas;
+
+import ull.patrones.paradas.iu.VentanaPrincipal;
+
+public class Practica8DAP
+{
+	public static void main(String[] args)
+	{
+		
+		new VentanaPrincipal();
+	}
+}
