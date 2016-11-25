448cf006337a78c69143f9eac1897b635c51fe27
Orlandy Ariel <alu0100773408@ull.edu.es>
2016-11-25 13:44:01 +0000

    clase(sin codigo aun) para representar el mapa

---
+++ src/ull/patrones/paradas/mapa/Mapa.java
@@ -0,0 +1,12 @@
+package ull.patrones.paradas.mapa;
+
+public class Mapa
+{
+	private Double m_lon;
+	private Double m_lat;
+	public Mapa(Double lon, Double lat)
+	{
+		m_lat =lat;
+		m_lon = lat;
+	}
+}
