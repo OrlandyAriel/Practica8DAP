125e1f16d28e4d8ba4208d4c82c7666dae33518d
Orlandy Ariel Sánchez <alu0100773408@ull.edu.es>
2016-11-25 18:20:21 +0000

    Mapa listo,@TODO:falta mejorar la integración entre el mapa y la ventana

--- src/ull/patrones/paradas/iu/VentanaPrincipal.java
+++ src/ull/patrones/paradas/iu/VentanaPrincipal.java
@@ -11,16 +11,25 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JRadioButton;
 
+import com.teamdev.jxbrowser.chromium.Browser;
+import com.teamdev.jxbrowser.chromium.swing.BrowserView;
+
+import ull.patrones.paradas.mapa.Mapa;
+
 public class VentanaPrincipal extends JFrame
 {
+	private static final long serialVersionUID = -8439451759008893300L;
 	private JRadioButton m_RJParadasGuaguas;
 	private JRadioButton m_RJParadasTaxis;
 	private JPanel m_panelSuperior;
 	private JPanel m_panelCentral;
-	
+
 	private JComboBox<String> m_JCParadaGuaguas;
 	private JComboBox<String> m_JCParadaTaxis;
 	private ButtonGroup m_BGRadios;
+	private Browser m_browser;
+	private BrowserView m_browserView;
+
 	public VentanaPrincipal()
 	{
 		configRadios();
@@ -30,6 +39,7 @@ public class VentanaPrincipal extends JFrame
 		configPanelCentral();
 		initComponent();
 	}
+
 	/**
 	 * Método para configurar el panel superior
 	 */
@@ -37,69 +47,76 @@ public class VentanaPrincipal extends JFrame
 	{
 		m_panelSuperior = new JPanel();
 		m_panelSuperior.setLayout(new FlowLayout());
-		
+
 		JPanel t_panelIzquierda = new JPanel();
 		t_panelIzquierda.setLayout(new BoxLayout(t_panelIzquierda, BoxLayout.Y_AXIS));
 		t_panelIzquierda.add(m_RJParadasTaxis);
-		t_panelIzquierda.add(m_JCParadaTaxis,Box.LEFT_ALIGNMENT);
-		
+		t_panelIzquierda.add(m_JCParadaTaxis, Box.LEFT_ALIGNMENT);
+
 		JPanel t_panelDerecha = new JPanel();
 		t_panelDerecha.setLayout(new BoxLayout(t_panelDerecha, BoxLayout.Y_AXIS));
 		t_panelDerecha.add(m_RJParadasGuaguas);
 		t_panelDerecha.add(m_JCParadaGuaguas);
-		
-		m_panelSuperior.add(t_panelIzquierda,FlowLayout.LEFT);
-		m_panelSuperior.add(t_panelDerecha,FlowLayout.LEFT);
+
+		m_panelSuperior.add(t_panelIzquierda, FlowLayout.LEFT);
+		m_panelSuperior.add(t_panelDerecha, FlowLayout.LEFT);
 	}
+
 	/**
 	 * Método para crear los combobox y añadirle sus datos
 	 */
 	private void configCombos()
 	{
-		String [] t_barrios = obtenerBarrios();
+		String[] t_barrios = obtenerBarrios();
 		m_JCParadaTaxis = new JComboBox<String>(t_barrios);
 		m_JCParadaTaxis.setVisible(true);
 		m_JCParadaTaxis.setEnabled(false);
-		
+
 		m_JCParadaGuaguas = new JComboBox<String>(t_barrios);
 		m_JCParadaGuaguas.setVisible(true);
 		m_JCParadaGuaguas.setEnabled(false);
 	}
+
 	private void configMapa()
 	{
-		
+		m_browser = new Browser();
+		m_browserView = new BrowserView(m_browser);
 	}
+
 	/**
 	 * Método para configurar el panel central, donde irá el mapa
 	 */
 	private void configPanelCentral()
 	{
 		m_panelCentral = new JPanel();
-		m_panelCentral.setLayout(new FlowLayout());
 		m_panelCentral.setVisible(true);
+		m_panelCentral.add(m_browserView);
 	}
-	
+
 	private String[] obtenerBarrios()
 	{
-		String []t_result={"Barrio la salud","Taco","cruz del señor"};
+		String[] t_result =
+		{ "Barrio la salud", "Taco", "cruz del señor" };
 		return t_result;
 	}
+
 	/**
 	 * Método que configura los radiosButton
 	 */
 	private void configRadios()
 	{
 		m_BGRadios = new ButtonGroup();
-		
+
 		m_RJParadasTaxis = new JRadioButton("Paradas de Taxis");
 		m_RJParadasTaxis.setVisible(true);
-		
+
 		m_RJParadasGuaguas = new JRadioButton("Paradas de Guaguas");
 		m_RJParadasGuaguas.setVisible(true);
-		
+
 		m_BGRadios.add(m_RJParadasTaxis);
 		m_BGRadios.add(m_RJParadasGuaguas);
 	}
+
 	/**
 	 * Método para añadir los componentes a la ventana principal y configurarla
 	 */
@@ -113,7 +130,23 @@ public class VentanaPrincipal extends JFrame
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		this.setLocationRelativeTo(null);
 
-		this.add(m_panelSuperior,BorderLayout.NORTH);
-		this.add(m_panelCentral,BorderLayout.CENTER);
+		this.add(m_panelSuperior, BorderLayout.NORTH);
+		configPanelCentral();
+		this.add(m_browserView, BorderLayout.CENTER);
+		double lat = 28.445665227752624;
+		double lng = -16.308826310768087;
+		Thread a = new Thread()
+		{
+			@Override
+			public void run()
+			{
+				Mapa t = new Mapa(lat, lng);
+				System.err.println(t.getMapa());
+				m_browser.loadHTML(t.getMapa());
+			}
+
+		};
+		a.start();
+
 	}
 }
--- src/ull/patrones/paradas/mapa/Mapa.java
+++ src/ull/patrones/paradas/mapa/Mapa.java
@@ -2,11 +2,21 @@ package ull.patrones.paradas.mapa;
 
 public class Mapa
 {
-	private Double m_lon;
+	private Double m_lng;
 	private Double m_lat;
-	public Mapa(Double lon, Double lat)
+
+	public Mapa(Double lat, Double lng)
 	{
-		m_lat =lat;
-		m_lon = lat;
+		m_lng = lng;
+		m_lat = lat;
+	}
+
+	/**
+	 * Método que configura el javascript y el mapa
+	 *  @return,devuelve un string con el código javascript y html del mapa
+	 */
+	public String getMapa()
+	{
+		return "<!DOCTYPE html>\r\n<html>\r\n  <head>\r\n    <style type=\"text/css\">\r\n      html, body { height: 100%; margin: 0; padding: 0; }\r\n      #map { height: 100%; }\r\n    </style>\r\n  </head>\r\n  <body>\r\n    <div id=\"map\"></div>\r\n    <script type=\"text/javascript\">\r\nvar map;\r\nfunction initMap() {\r\n \r\n  var parada = {lat: "+m_lat+", lng: "+m_lng+"};\r\n  var map = new google.maps.Map(document.getElementById('map'), {\r\n    zoom: 15,\r\n    center: parada\r\n  });\r\n\r\n  var marker = new google.maps.Marker({\r\n    position: parada,\r\n    map: map,\r\n    title: 'Parada de taxi'\r\n  });\r\n}\r\nmarker.setMap(map);\r\n    </script>\r\n    <script async defer\r\n      src=\"https://maps.googleapis.com/maps/api/js?key=AIzaSyBKLG4--vt2LLBG4Kz8K0qfCynzrBcNwdc&callback=initMap\">\r\n    </script>\r\n  </body>\r\n</html>";
 	}
 }
